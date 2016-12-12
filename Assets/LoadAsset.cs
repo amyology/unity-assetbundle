9c6de5733f401bb44db481651b3e3fe0328fd8b4
Amy Chan <chamy09@gmail.com>
2016-12-12 15:37:14 -0600

    initial commit

---
+++ Assets/LoadAsset.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssetBundles;
+using System;
+
+public class LoadAsset : MonoBehaviour {
+
+	public string BundleURL;
+	public string AssetName;
+
+	IEnumerator Start () {
+
+		using (WWW www = new WWW(BundleURL)) {
+			yield return www;
+			if (www.error != null)
+				throw new Exception("WWW download had an error:" + www.error);
+			AssetBundle bundle = www.assetBundle;
+			if (AssetName == "")
+				Instantiate(bundle.mainAsset);
+			else
+				Instantiate(bundle.LoadAsset(AssetName));
+
+			bundle.Unload(false);
+
+		}
+	}
+}
